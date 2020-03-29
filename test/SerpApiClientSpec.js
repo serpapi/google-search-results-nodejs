10aadf89a96c9940eae666b47ae555a0a425dd23
jvmvik <jvmvik@gmail.com>
2020-03-29 18:57:46 -0500

    SerpApiClientSpec test only SerpApiClient class

--- test/SerpApiClientSpec.js
+++ test/SerpApiClientSpec.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const serpapi = require('../lib/GoogleSearchResults');
+const serpapi = require('../lib/SerpApiClient');
 
 describe('Google Search Results', () => {
   let p, api_key;
@@ -14,27 +14,27 @@ describe('Google Search Results', () => {
   });
 
   it('fail:buildUrl', (done) => {
-    let client = new serpapi.GoogleSearchResults()
+    let client = new serpapi.SerpApiClient(null)
     expect(() => {
       client.buildUrl('/path', {}, "json", null)
     }).toThrow(/api_key/)
     done()
-  })
+  }).timeout(10000)
 
   it('buildUrl', (done) => {
-    let client = new serpapi.GoogleSearchResults('demo')
+    let client = new serpapi.SerpApiClient('demo')
     expect(client.buildUrl('/path', { q: 'Coffee', location: 'Austin, Texas', api_key: 'beta' }, "json")).toMatch(/https:\/\/serpapi.com\/path\?q=Coffee&location=Austin%2C%20Texas&api_key=beta&source=nodejs&output=json/)
     done()
-  })
+  }).timeout(10000)
 
   it('buildUrl without api_key', (done) => {
-    let client = new serpapi.GoogleSearchResults('demo')
+    let client = new serpapi.SerpApiClient('demo')
     expect(client.buildUrl('/path', { q: 'Coffee', location: 'Austin, Texas' }, "json")).toMatch(/https:\/\/serpapi.com\/path\?q=Coffee&location=Austin%2C%20Texas&source=nodejs&output=json&api_key=demo/)
     done()
-  })
+  }).timeout(10000)
 
   it("search", (done) => {
-    let client = new serpapi.GoogleSearchResults(api_key)
+    let client = new serpapi.SerpApiClient(api_key, "google")
     client.setTimeout(6000);
     client.search(p, "json", (raw) => {
       let data = JSON.parse(raw)
@@ -42,34 +42,22 @@ describe('Google Search Results', () => {
       expect(data.organic_results[0].title.length).toBeGreaterThan(5)
       done()
     })
-  })
+  }).timeout(10000)
 
   it("json", (done) => {
-    let client = new serpapi.GoogleSearchResults(api_key)
+    let client = new serpapi.SerpApiClient(api_key, "google")
     client.json(p, (data) => {
       expect(data.organic_results[0].title.length).toBeGreaterThan(5)
       done()
     })
-  })
+  }).timeout(10000)
 
   it("html", (done) => {
-    let client = new serpapi.GoogleSearchResults(api_key)
+    let client = new serpapi.SerpApiClient(api_key, "google")
     client.html(p, (body) => {
       expect(body).toMatch(/<\/html>/)
       done()
     })
-  })
+  }).timeout(10000)
 
-  xit("fail:json", (done) => {
-    try {
-      let client = new serpapi.GoogleSearchResults(api_key)
-      let fn = (data) => {
-        done()
-      }
-      client.json({}, fn)
-    } catch (ex) {
-      expect(ex.message).toBe("Error: Missing query `q` parameter")
-      done()
-    }
-  })
 });
